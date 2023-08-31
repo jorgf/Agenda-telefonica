5d963c4a4b9dab1709ccc15a49dac2b6eda86037
Jorge França <jorgeartur.f@gmail.com>
2023-08-30 23:19:53 -0300

    Atualizacao do metodo remove()

--- classes/Schedule.rb
+++ classes/Schedule.rb
@@ -2,20 +2,20 @@ require_relative "Files.rb"
 
 class Schedule
   def initialize
-    @schedule = Files.new("./contacts.json")
+    @file = Files.new("./contacts.json")
   end
 
   def add(name, phone)
     data = {"name" => name, "phone"=> phone}
-    @schedule.write_file(data)
+    @file.write_file(data)
   end
 
   def list
-    @schedule.read_file
+    @file.read_file
   end
 
   def edit(name) #TODO
-    @schedule.each do |contact|
+    @file.each do |contact|
       if contact[:name].downcase == name.downcase
         temp = contact[:name]
         puts "novo nome (Enter para manter o atual): "
@@ -33,10 +33,12 @@ class Schedule
   end
 
   def remove(name) #TODO
-    @schedule.each do |contact|
-      if contact[:name].downcase == name.downcase
-        index = @schedule.index(contact)
-        return @schedule.delete_at(index)
+    @data_hash = @file.read_file
+    for i in @data_hash
+      if i["name"].downcase == name.downcase
+        index = @data_hash.index(i)
+        @data_hash.delete_at(index)
+        @file.update_file(@data_hash)
       end
     end
   end
