07cfe45dd8a147e603bce07a870c36364fda38f4
Jorge França <jorgeartur.f@gmail.com>
2023-08-30 23:18:59 -0300

    construcao do metodo update_file

--- classes/Files.rb
+++ classes/Files.rb
@@ -6,15 +6,20 @@ class Files
     file = File.read(path)
     @data_hash = JSON.parse(file)
   end
-
+  
   def read_file
     @data_hash["contacts"]
   end
-
+  
   def write_file(data)
     data_save = data
     @data_hash["contacts"] << data_save
-    File.write('./contacts.json', JSON.pretty_generate(@data_hash,{indent:'  '}))
+    File.write('./contacts.json', JSON.pretty_generate(@data_hash,{indent:' '}))
+  end
+  
+  def update_file(data)
+    @data_hash["contacts"] = data
+    File.write('./contacts.json', JSON.pretty_generate(@data_hash,{indent:' '}))
   end
   
 end
