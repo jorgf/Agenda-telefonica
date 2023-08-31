0bf490aefa33988606da87e185796be66d62f616
Jorge França <jorgeartur.f@gmail.com>
2023-08-31 09:46:41 -0300

    Correcao do metodo edit()

--- classes/Schedule.rb
+++ classes/Schedule.rb
@@ -14,31 +14,32 @@ class Schedule
     @file.read_file
   end
 
-  def edit(name) #TODO
-    @file.each do |contact|
-      if contact[:name].downcase == name.downcase
-        temp = contact[:name]
+  def edit(name)
+    data_hash = @file.read_file
+    data_hash.each do |contact|
+      if contact["name"].downcase == name.downcase
+        temp_name = contact["name"]
         puts "novo nome (Enter para manter o atual): "
         new_name = gets.chomp
-        contact[:name] = new_name
-        contact[:name] = contact[:name].empty? ? temp : contact[:name]
-        
-        temp_phone = contact[:phone]
+        contact["name"] = new_name
+        contact["name"] = contact["name"].empty? ? temp_name : contact["name"]
+        temp_phone = contact["phone"]
         puts "novo numero (Enter para manter o atual): "
         new_phone = gets.chomp
-        contact[:phone] = new_phone
-        contact[:phone] = contact[:phone].empty? ? temp_phone : contact[:phone]
+        contact["phone"] = new_phone
+        contact["phone"] = contact["phone"].empty? ? temp_phone : contact["phone"]
       end
+    @file.update_file(data_hash)
     end
   end
 
-  def remove(name) #TODO
-    @data_hash = @file.read_file
-    for i in @data_hash
+  def remove(name)
+    data_hash = @file.read_file
+    for i in data_hash
       if i["name"].downcase == name.downcase
-        index = @data_hash.index(i)
-        @data_hash.delete_at(index)
-        @file.update_file(@data_hash)
+        index = data_hash.index(i)
+        data_hash.delete_at(index)
+        @file.update_file(data_hash)
       end
     end
   end
